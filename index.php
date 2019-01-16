393c8e75437eee10c65cef1fffd1ccc09c39f480
fachrul2896 <46764451+fachrul2896@users.noreply.github.com>
2019-01-17 06:35:34 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<table width="337">
+
+<td width="298" height="300" align="center"> <p><strong> NAMA : Andi Fachrul Rizal </strong> </p>
+	<p><strong> NIM : 215280011 </strong></p>
+	<p><strong> PHONE : 082191986732 </strong></p>
+	<p><strong> EMAIL : Fachrulandi47@gmail.com </strong></p>
+	<p><strong> South Sulawesi </strong></p> 
